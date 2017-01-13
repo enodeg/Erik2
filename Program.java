2847150342e719f58e2d233298796851123fabc0
Erik <enodeg@gmail.com>
2017-01-13 17:30:38 +0200

    Signed-off-by: Erik <enodeg@gmail.com>

---
+++ Program.java
@@ -0,0 +1,15 @@
+public class Program{
+	public static void main (String[]  args){
+		Program program = new Program(){
+			program.start();
+		}
+		public void start(){
+			MyReader reader = new MyReader();
+			int a = reader.read();
+			int b = reader.read();
+			MyWorker worker = new MyWorker ();
+			worker.calcSum(a,b);
+			System.out.println(worker.getResult());
+		}
+	}
+}
