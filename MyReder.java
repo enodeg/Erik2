c8753365443c5ee1ad404881395b20dd8d61260e
Ogienko Bogdan <bogienko@mail.ru>
2017-01-13 17:59:21 +0200

    good1

---
+++ MyReder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+	Public class MyReder
+	{
+	Scanner scanner;
+
+	public MyReader ()
+	{
+	Scanner = new Scanner (System.in);
+	}
+
+	public int read ()
+	{
+	return scanner.nextInt ();
+	}
+}
